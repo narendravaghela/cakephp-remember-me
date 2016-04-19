ef0affa96e5d4367bd812562985d9113cff929bc
Narendra Vaghela <narendravaghela4389@gmail.com>
2016-04-19 18:07:09 +0530

    Add component, composer.json, readme and default test case

---
+++ tests/TestCase/Controller/Component/RememberMeComponentTest.php
@@ -0,0 +1,54 @@
+<?php
+namespace RememberMe\Test\TestCase\Controller\Component;
+
+use Cake\Controller\ComponentRegistry;
+use Cake\TestSuite\TestCase;
+use RememberMe\Controller\Component\RememberMeComponent;
+
+/**
+ * RememberMe\Controller\Component\RememberMeComponent Test Case
+ */
+class RememberMeComponentTest extends TestCase
+{
+
+    /**
+     * Test subject
+     *
+     * @var \RememberMe\Controller\Component\RememberMeComponent
+     */
+    public $RememberMe;
+
+    /**
+     * setUp method
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $registry = new ComponentRegistry();
+        $this->RememberMe = new RememberMeComponent($registry);
+    }
+
+    /**
+     * tearDown method
+     *
+     * @return void
+     */
+    public function tearDown()
+    {
+        unset($this->RememberMe);
+
+        parent::tearDown();
+    }
+
+    /**
+     * Test initial setup
+     *
+     * @return void
+     */
+    public function testInitialization()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+}
